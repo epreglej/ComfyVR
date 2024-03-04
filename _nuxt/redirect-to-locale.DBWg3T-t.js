f19f9de30060cddf44780de23c0ebf9ea29ff4a3
epreglej <133610849+epreglej@users.noreply.github.com>
2024-03-04 16:46:25 +0000

    Deploying to gh-pages from @ epreglej/epreglej.github.io@140796f0861f968f3b6abdad247dd9df134cffed 🚀

---
+++ _nuxt/redirect-to-locale.DBWg3T-t.js
@@ -0,0 +1,1 @@
+import{O as r,g as t,L as e}from"./entry.DKMVsvg_.js";import{c as i}from"./languages.BB-x9giw.js";const u=r((a,s)=>{const o=t("locale",{sameSite:"none",secure:!0});if(a.path==="/")return o.value==i.code?e("/hr"):e("/br")});export{u as default};
