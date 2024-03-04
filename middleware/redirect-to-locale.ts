5af0cb2da436e46463c67499efbe5d4fe9101d88
ePreglej <ep53583@fer.hr>
2024-03-04 17:59:40 +0100

    Trying to fix cookies again.

--- middleware/redirect-to-locale.ts
+++ middleware/redirect-to-locale.ts
@@ -4,11 +4,15 @@ export default defineNuxtRouteMiddleware((to, from) => {
         secure: true,
     });
 
+    console.log("Redirecting...");
     if (to.path === "/") {
+        console.log(locale.value);
+        console.log(croatianLanguage.code);
+        console.log(locale.value == croatianLanguage.code);
         if (locale.value == croatianLanguage.code) {
-            return navigateTo("/hr");
+            //return navigateTo("/hr");
         } else {
-            return navigateTo("/en");
+            //return navigateTo("/en");
         }
     }
 });
