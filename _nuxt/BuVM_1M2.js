df22a318294b6d5a3d841422853302493579f01e
epreglej <133610849+epreglej@users.noreply.github.com>
2024-03-19 19:28:00 +0000

    Deploying to gh-pages from @ epreglej/epreglej.github.io@db8b7642d365bc920738ad4cf8f268f41545e0db 🚀

---
+++ _nuxt/BuVM_1M2.js
@@ -0,0 +1,1 @@
+import{c as e,o}from"./DxJ8-sY_.js";const t={class:"h-screen w-full bg-gradient-to-r from-blue-400 via-blue-200 to-neutral-0"},l={__name:"login",setup(r){return(n,a)=>(o(),e("div",t," Hi from login.vue! "))}};export{l as default};
