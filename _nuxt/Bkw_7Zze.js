6f47df8240c18e6e0daf470e9bc668dc86fe3d84
epreglej <133610849+epreglej@users.noreply.github.com>
2024-03-22 15:19:32 +0000

    Deploying to gh-pages from @ epreglej/epreglej.github.io@72ce6376a253416478371e8c98a3c69c979c36aa 🚀

---
+++ _nuxt/Bkw_7Zze.js
@@ -0,0 +1,1 @@
+import{R as t,S as s}from"./ByEq0mKC.js";const c={__name:"logout",setup(n){const e=t();async function r(){try{const{error:o}=await e.auth.signOut();o?console.error("Logout error!"):(console.log("Logout successful!"),await s("/login"))}catch(o){console.error("Unexpected error:",o)}}return r(),()=>{}}};export{c as default};
