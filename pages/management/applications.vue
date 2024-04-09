3b9906e6e74830a543e4a985abd54216ec2eb12a
Eugen Preglej <ep53583@fer.hr>
2024-04-09 11:36:02 +0200

    Added blank login.vue to fix deployment errors.

--- pages/management/applications.vue
+++ pages/management/applications.vue
@@ -7,23 +7,6 @@ definePageMeta({
 
 <template>
     <div class="main responsive page right active">
-        <div>
-            <div class="tabs">
-                <a href="#" class="active" data-ui="#add">
-                    <i>home</i>
-                    <span>Add a new app</span>
-                </a>
-                <a href="#" data-ui="#update">
-                    <i>home</i>
-                    <span>Update an existing app</span>
-                </a>
-            </div>
-            <div id="add" class="page padding active left">
-                <h5>Tab 1</h5>
-            </div>
-            <div id="update" class="page padding right">
-                <h5>Tab 2</h5>
-            </div>
-        </div>
+        <div></div>
     </div>
 </template>
